ff8f83245df714c1de2027cc092f4eaf510d0d0a
root <root@localhost.localdomain>
2017-06-07 22:38:07 -0400

    update

--- codis-2/docker-entrypoint.sh
+++ codis-2/docker-entrypoint.sh
@@ -21,13 +21,13 @@ cat > /opt/local/codis/config/config.ini << EOF
 ##################################################
 
 # Set Coordinator, only accept "zookeeper" & "etcd" & "filesystem".
-coordinator = "${coord_name}"
-zk= "${coord_list}"
+coordinator=${coord_name}
+zk=${coord_list}
 
 # Set Codis Product Name/Auth.
-product = "${prd_name}"
-dashboard_addr = "${prd_dashboard}:18087"
-password = "${prd_auth}"
+product=${prd_name}
+dashboard_addr=${prd_dashboard}:18087
+password=${prd_auth}
 
 # Set configs.
 backend_ping_period=5
@@ -63,13 +63,13 @@ cat > /opt/local/codis/config/config.ini << EOF
 ##################################################
 
 # Set Coordinator, only accept "zookeeper" & "etcd" & "filesystem".
-coordinator = "${coord_name}"
-zk= "${coord_list}"
+coordinator=${coord_name}
+zk=${coord_list}
 
 # Set Codis Product Name/Auth.
-product = "${prd_name}"
-dashboard_addr = "${prd_dashboard}:18087"
-password = "${prd_auth}"
+product=${prd_name}
+dashboard_addr=${prd_dashboard}:18087
+password=${prd_auth}
 
 # Set configs.
 backend_ping_period=5
@@ -77,7 +77,7 @@ session_max_timeout=1800
 session_max_bufsize=131072
 session_max_pipeline=1024
 zk_session_timeout=30000
-proxy_id= "${prd_proxy}"
+proxy_id=${prd_proxy}
 EOF
 
 echo "-------------------------------------------------------------------------------"
@@ -85,7 +85,7 @@ echo "------------------------------proxy config file---------------------------
 cat /opt/local/codis/config/config.ini
 echo "-------------------------------------------------------------------------------"
 
-CMD="/opt/local/codis/bin/codis-proxy --log-level info -c /opt/local/codis/config/config.ini -L /opt/local/codis/logs/proxy.log  --cpu=4 --addr=${prd_proxy}:19000 --http-addr=${prd_proxy}:11000"
+CMD="/opt/local/codis/bin/codis-proxy --log-level info -c /opt/local/codis/config/config.ini -L /opt/local/codis/logs/proxy.log  --cpu=4 --addr=0.0.0.0:19000 --http-addr=0.0.0.0:11000"
 
 elif [ $CODIS_TYPE = "codis_server" ];
 then
